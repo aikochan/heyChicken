e2fb2d2e71a39c633aacffad65422adb7d6f470f
Alison Hughes <djaiko1@gmail.com>
2015-01-03 22:21:21 -0700

    Moved device search out of setup to loop so that if it fails, it can be retried. Properly implemented CRC per the datasheet so that it checks the read data integrity. Debug logging to be removed soon.

--- heyChicken.ino
+++ heyChicken.ino
@@ -1,35 +1,59 @@
 #include <OneWire.h> 
 
-#define MAX_DS1820_SENSORS 2
+#define MAX_DS1820_SENSORS  2
+#define DS18S20_Pin         2 
 
-int DS18S20_Pin = 2; //DS18S20 Signal pin on digital 2
+boolean foundAllDevices = false;
+byte addr[MAX_DS1820_SENSORS][8];
 
-//Temperature chip i/o
 OneWire ds(DS18S20_Pin);    // on digital pin 2
 
-byte addr[MAX_DS1820_SENSORS][8];
+void errorMessage(char *msg)
+{
+  
+}
 
 void setup(void) 
 {
-    Serial.begin(9600);
-    if (!ds.search(addr[0])) 
+    Serial.begin(9600);  
+}
+
+boolean findDS18S20Devices(void)
+{
+    boolean success = true;
+    for (int sensor = 0; sensor < MAX_DS1820_SENSORS; sensor++)
     {
-        //no more sensors on chain, reset search
-        ds.reset_search();
-        delay(250);
-        return;
+        if (!ds.search(addr[sensor])) 
+        {
+            Serial.print("Can't find DS18S20 Device ID: ");
+            Serial.println(sensor);
+            success = false;
+        } else if (OneWire::crc8(addr[sensor], 7) != addr[sensor][7]) {
+            Serial.println( "CRC is not valid" );
+            success = false;
+        } else if (addr[sensor][0] != 0x28) {
+            Serial.print("Device is not a DS18S20 family device. Device ID: ");
+            Serial.println(sensor);
+            success = false;
+        }
+        if (!success)
+        {
+            //reset search for next try
+            ds.reset_search();
+            delay(250);
+            break;
+        }
     }
-    if (!ds.search(addr[1])) 
-    {
-        //no more sensors on chain, reset search
-        ds.reset_search();
-        delay(250);
-        return;
-    }
+    return success;
 }
 
 void loop(void) 
 {
+    if (!foundAllDevices)
+    {
+        if (!(foundAllDevices = findDS18S20Devices())) return;
+    } 
+    
     float temp;
     for (int sensor = 0; sensor < MAX_DS1820_SENSORS; sensor++)
     {
@@ -46,10 +70,11 @@ void loop(void)
         }
         delay(1000);
     }
+
 }
 
 // obtains the temperature from one DS18S20 in DEG Celsius as result
-// returns true on success, false on error
+// returns true on success, false on sensor index out of bounds or CRC check failed on read
 boolean getTemp(int sensor, float *result)
 {
     boolean success = true;
@@ -57,19 +82,9 @@ boolean getTemp(int sensor, float *result)
     
     if (sensor >= MAX_DS1820_SENSORS)
     {
-        Serial.println( "Sensor index invalid" );
+        Serial.println("Sensor index invalid");
         success = false;
-    } else if (OneWire::crc8( addr[sensor], 7) != addr[sensor][7]) {
-        Serial.println( "CRC is not valid" );
-        success = false;
-    } else if (addr[sensor][0] != 0x28) {
-        Serial.print("Device is not a DS18S20 family device. Device ID: ");
-        Serial.println(addr[sensor][0]);
-        success = false;
-    }
-
-    if (success) 
-    {
+    } else {
         ds.reset();
         ds.select(addr[sensor]);
         ds.write(0x44,1);    // start conversion
@@ -80,14 +95,22 @@ boolean getTemp(int sensor, float *result)
         ds.select(addr[sensor]);        
         ds.write(0xBE);        // Read Scratchpad
     
-        for (int i = 0; i < 9; i++) 
+        for (int i = 0; i < 9; i++)
         {                        
-            // we need 9 bytes
             data[i] = ds.read();
         }
-    
-        float tempRead = ((data[1] << 8) | data[0]);     //using two's compliment (???)
-        *result = (tempRead / 16);
+        
+        // CRC check on read data
+        if (OneWire::crc8(data, 8) != data[8])
+        {
+            Serial.println("Data CRC failed");
+            Serial.println(OneWire::crc8(data, 8));
+            Serial.println(data[8]);
+            success = false;
+        } else {
+            float tempRead = ((data[1] << 8) | data[0]);     //using two's compliment (???)
+            *result = (tempRead / 16);
+        }
     }
     return success;
 }
