f66688bfc409219802cf4cd94f8a20a36e642e2d
Alison Hughes <djaiko1@gmail.com>
2015-02-27 15:53:34 -0700

    Fixed #10 added tuning to utility

--- heyChickenUtility.py
+++ heyChickenUtility.py
@@ -58,9 +58,31 @@ HEATER_STATUS = 4
 status_vars = None
 
 # for reading and setting tuning values
-# index 0 is doing nothing at the moment, makes accessing tokens easier since the message type is sitting in that spot
-# packet format: type | light | pressure | heater on | heater off | smoothing (as an integer 0-100)
-tuning_parameters = [0, 0, 0, 0, 0, 0]
+tuning_parameters = None
+
+def signal_handler(signal, frame):
+	print "\n******** Received SIGINT *************"
+	sys.exit(0)
+
+# def operate_door():
+
+# def operate_heat():
+
+#def check_heater(heaterStatus):
+
+def send_udp_message(msg, client_socket):
+	global udp_lock
+	reply = None
+	with udp_lock:
+		bytes_sent = client_socket.sendto(msg, (ARDUINO_IPADDRESS, UDP_PORT))
+		if bytes_sent:
+			try:
+				reply, server_address_info = client_socket.recvfrom(1024)
+			except socket.timeout:
+				print "\nError: UDP receive timeout in send_udp_message"
+		else:
+			print "\nError: UDP sendto timeout in send_udp_message"
+	return reply		 
 
 def receive_status(tokens):
 	global status_lock, status_vars
@@ -71,68 +93,20 @@ def receive_status(tokens):
 		fileHandle.write("{} {}\n".format(int(time.time()), " ".join(tokens[1:-1])))
 		fileHandle.close()
 	
-def receive_tuning(tokens):
-	global tuning_parameters
-	# this should be re-written as the indexes are sequential
-	tuning_parameters[LIGHT_THRESHOLD] = int(tokens[LIGHT_THRESHOLD])
-	tuning_parameters[PRESSURE_THRESHOLD] = int(tokens[PRESSURE_THRESHOLD])
-	tuning_parameters[TEMP_HEATER_ON] = int(tokens[TEMP_HEATER_ON])
-	tuning_parameters[TEMP_HEATER_OFF] = int(tokens[TEMP_HEATER_OFF])
-	tuning_parameters[SMOOTHING_FACTOR] = int(tokens[SMOOTHING_FACTOR])
-		
-def handle_error(tokens):
-	print "Error:"
-	print tokens
-	
-available_actions = {MSG_STATUS: receive_status,
-					 MSG_TUNING: receive_tuning,
-					 MSG_ERROR: handle_error}
-		
-# def operate_door():
-
-# def operate_heat():
-
-#def check_heater(heaterStatus):
-
-def process_message(data):
-	tokens = data.split()
-	if tokens:
-		action = tokens[0]
-		available_actions.get(action, lambda: None)(tokens)
-	else:
-		print "Empty message"
-	
-def send_udp_message(msg, client_socket):
-	global udp_lock
-	reply = None
-	with udp_lock:
-		bytes_sent = client_socket.sendto(msg, (ARDUINO_IPADDRESS, UDP_PORT))
-		if bytes_sent:
-			try:
-				reply, server_address_info = client_socket.recvfrom(1024)
-			except socket.timeout:
-				print "Error: UDP receive timeout in send_udp_message"
-		else:
-			print "Error: UDP sendto timeout in send_udp_message"
-	return reply		 
-
-# does not acquire lock	
 def request_status(client_socket):
 	# print "current thread: ", threading.current_thread()
 	# print "thread active count: ", threading.active_count()
 	incoming_msg = send_udp_message(MSG_REQ_STATUS, client_socket)
 	if incoming_msg is not None:
-		process_message(incoming_msg)
+		tokens = incoming_msg.split()
+		if tokens:
+			receive_status(tokens)
 	global lets_shutdown, timer	
 	if not lets_shutdown:
 		timer = Timer(STATUS_POLLING_INTERVAL, request_status, [client_socket,])
 		timer.start()
 	else:
 		sys.exit(0)
-
-def signal_handler(signal, frame):
-	print "\n******** Received SIGINT *************"
-	sys.exit(0)
 
 def print_status(client_socket):
 	if status_vars is not None:
@@ -145,11 +119,36 @@ def print_status(client_socket):
 			print "{} °F\tlight ({})".format(status_vars[TEMP_RUN], status_vars[LIGHT])
 			print "**************************************\n"
 	else:
-		print "No status retrieved from Arduino yet."
+		print "\nNo status retrieved from Arduino yet."
 
-def tune_parameters(client_socket):
-	print "tune_parameters"
+def print_parameters(parameters):
+		print "Current tuning parameters:"
+		print "1: Light [0-1000]\t{}".format(parameters[LIGHT_THRESHOLD])
+		print "2: Pressure [0-1000]\t{}".format(parameters[PRESSURE_THRESHOLD])
+		print "3: Heater ON [0-40]\t{}".format(parameters[TEMP_HEATER_ON])
+		print "4: Heater OFF [0-70]\t{}".format(parameters[TEMP_HEATER_OFF])
+		print "5: Smoothing [0-100]\t{}".format(parameters[SMOOTHING_FACTOR])
 	
+def tune_parameters(client_socket):
+	print "\n...fetching current parameters...\n"
+	incoming_msg = send_udp_message(MSG_REQ_TUNING, client_socket)
+	if incoming_msg is not None:
+		parameters = incoming_msg.split()
+		print_parameters(parameters)
+		parameterType = int(input('\nParameter to tune (1-5): '))
+		parameterValue = input('Enter the new value: ')
+		print "\n...transmitting...\n"	
+		parameters[parameterType] = parameterValue;
+		message = "%c %s %s %s %s %s " % (MSG_SET_TUNING, parameters[LIGHT_THRESHOLD], parameters[PRESSURE_THRESHOLD], parameters[TEMP_HEATER_ON], parameters[TEMP_HEATER_OFF], parameters[SMOOTHING_FACTOR])
+		incoming_msg = send_udp_message(message, client_socket)
+		if incoming_msg is not None:
+			parameters = incoming_msg.split()
+			print_parameters(parameters)
+		else:
+			print "\nArduino not responding with parameters."		
+	else:
+		print "\nArduino not responding with parameters."
+		
 def toggle_data_gathering(client_socket):
 	print "toggle_data_gathering"
 	
@@ -183,7 +182,6 @@ if __name__ == "__main__":
 	client_socket = None
 	data_gathering = True
 	tweeting = False
-	
 	
 	print "\n*** Hey! Chicken! Utility! ***"
 
